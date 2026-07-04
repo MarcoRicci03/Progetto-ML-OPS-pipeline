eacc17d38c9db8ff2fab7e13e1e1f343bb8a3145
paoloficaraa <paoloficara50@gmail.com>
2026-07-05 01:10:42 +0200

    ottimizzato la documentazione e la struttura della pipeline: aggiornati i commenti e migliorata la chiarezza dei nodi

--- run_pipeline.py
+++ run_pipeline.py
@@ -8,14 +8,13 @@ pipe = PipelineController(
     add_pipeline_tags=False
 )
 
-# 2. Parametri Globali (Prevenzione Data Drift)
-# In futuro, basterà cambiare questo URL per far girare tutto sui nuovi dati mensili
+# 2. Parametri Globali
 pipe.add_parameter(
     name='dataset_url', 
     default='https://d37ci6vzurychx.cloudfront.net/trip-data/yellow_tripdata_2024-01.parquet'
 )
 
-# 3. Nodo A: Pipeline di Marco (Ingestion & Baseline)
+# 3. NODO A: Pipeline di Marco (Ingestion & Baseline)
 pipe.add_step(
     name='step_marco_baseline',
     base_task_project='Progetto_MLOps_Esame',
@@ -23,20 +22,29 @@ pipe.add_step(
     parameter_override={'General/data_url': '${pipeline.dataset_url}'}
 )
 
-# 4. Nodo B: Pipeline di Paolo (Avanzato & Gatekeeper)
+# 4. NODO B: Pipeline di Paolo (Avanzato & Gatekeeper)
 pipe.add_step(
     name='step_paolo_xgboost',
-    parents=['step_marco_baseline'], # Il DAG forza l'attesa: Paolo parte SOLO se Marco finisce
+    parents=['step_marco_baseline'], # Attende Marco
     base_task_project='Progetto_MLOps_Esame',
     base_task_name='Pipeline_Paolo_XGBoost',
     parameter_override={
-        # Passaggio deterministico e nativo dell'ID tra i due nodi! Nessuna race condition.
         'Args/source_task_id': '${step_marco_baseline.id}' 
+    }
+)
+
+# 5. NODO C: Ottimizzazione Iperparametri (HPO)
+pipe.add_step(
+    name='step_hpo_tuning',
+    parents=['step_paolo_xgboost'], # Attende che Paolo finisca l'addestramento base
+    base_task_project='Progetto_MLOps_Esame',
+    base_task_name='Pipeline_HPO_XGBoost_Tuning', # Il nome esatto del task nel tuo script
+    parameter_override={
+        # Inietta l'ID del task di Paolo come "base_task_id" per l'ottimizzatore
+        'Args/base_task_id': '${step_paolo_xgboost.id}'
     }
 )
 
 if __name__ == '__main__':
     print("Avvio del PipelineController ClearML...")
-    # L'esecuzione locale è perfetta per le istanze di GitHub Actions.
-    # In azienda si userebbe pipe.start(queue='default') per delegare a cluster remoti con GPU.
     pipe.start_locally(run_pipeline_steps_locally=True)
